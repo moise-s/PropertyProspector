77f73d2458fb6798bff3e31a05f991a7c2ac8020
moise-s <moises.d.nasci@gmail.com>
2025-03-16 16:43:39 -0300

    init PropertyListing class

---
+++ PropertyProspector/models/models.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime, timedelta, timezone
+from decimal import Decimal
+
+
+class PropertyListing(BaseModel):
+    platform_id: str
+    description: str
+    price: Optional[Decimal]
+    condo_fee: Optional[Decimal]
+    location: Optional[str]
+    address: Optional[str]
+    url: Optional[str]
+    area: Optional[Decimal] = None
+    rooms: Optional[Decimal] = None
+    bathrooms: Optional[Decimal] = None
+    scraped_at: datetime = datetime.now(timezone(timedelta(hours=-3)))
