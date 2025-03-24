ff1b1b6e8c0775836a6527f9d2d978d64a45972e
moise-s <moises.d.nasci@gmail.com>
2025-03-24 14:40:59 -0300

    initial iteration on imovel_web + 1st version of bypassing cloudflare

--- PropertyProspector/websites/imovel_web.py
+++ PropertyProspector/websites/imovel_web.py
@@ -1,36 +1,67 @@
+import logging
+import asyncio
 from decimal import Decimal
 from datetime import datetime
-from typing import List
+from typing import Iterator, List, Optional
 from bs4 import BeautifulSoup
 from PropertyProspector.core.scraper import BaseScraper
 from PropertyProspector.models.models import PropertyListing, ScrapeSource
 from pydoll.browser.chrome import Chrome
+from pydoll.constants import By
 
 from PropertyProspector.utils.parser import extract_numeric_field, get_text
 
-URL = "https://www.imovelweb.com.br/apartamentos-venda-florianopolis-sc-desde-2-ate-3-quartos-200000-1000000-reales.html"
-# specific
-BASE_URL = "https://www.imovelweb.com.br/apartamentos-venda-trindade-florianopolis-itacorubi-corrego-grande-carvoeira-pantanal-florianopolis-santa-monica-florianopolis-coqueiros-florianopolis-estreito-florianopolis-abraao-capoeiras-florianopolis-kobrasol-campinas-sao-jose-desde-2-ate-3-quartos-200000-1000000-reales"
-URL = "{BASE_URL}.html"
-page = 1
-URL_PAGES = f"{BASE_URL}-pagina-{page}.html"
+_logger = logging.getLogger(__name__)
+
+# Silence INFO level logs from pydoll
+logging.getLogger('pydoll.connection.connection').setLevel(logging.WARNING)
 
 
 class ImovelWebScraper(BaseScraper):
-    # TODO - iterate over pages
-    # TODO - up to "Neste momento não temos imóveis com o perfil que está procurando" in html
-    async def scrape(self) -> str:
+    BASE_URL = (
+        "https://www.imovelweb.com.br/apartamentos-venda-trindade-florianopolis-itacorubi-corrego-grande-"
+        "carvoeira-pantanal-florianopolis-santa-monica-florianopolis-coqueiros-florianopolis-estreito-florianopolis-"
+        "abraao-capoeiras-florianopolis-kobrasol-campinas-sao-jose-desde-2-ate-3-quartos-200000-1000000-reales"
+    )
+
+    def pages_to_scrape(self) -> Iterator[str]:
+        page = 1
+        while True:
+            yield f"{self.BASE_URL}-pagina-{page}.html"
+            page += 1
+
+    async def scrape(self, url: str) -> Optional[str]:
+        _logger.info(f"Scraping URL: {url}")
         async with Chrome() as browser:
             await browser.start()
             page = await browser.get_page()
-            await page.go_to(URL_PAGES)
+            await page.go_to(url)
             await page._wait_page_load()
             page_html = await page.page_source
 
-            filename = f"{datetime.now().strftime('%d-%m-%Y-%H:%M:%S')}"
+            filename = datetime.now().strftime("%d-%m-%Y-%H:%M:%S")
             with open(f"{filename}.html", "w", encoding="utf-8") as f:
                 f.write(page_html)
 
+            if "<title>Just a moment...<" in page_html:
+                _logger.info("Got a cloudflare, waiting for its own handling!")
+                await asyncio.sleep(5)
+                await page._wait_page_load()
+                page_html = await page.page_source
+                if not ">Buscar imobiliárias<" in page_html:
+                    _logger.info("Couldn't bypass Cloudflare!")
+                    checkbox = await page.find_element(By.XPATH, '/html/body//div[1]')
+                    await checkbox.click()
+                    await asyncio.sleep(5)
+                    await page._wait_page_load()
+                with open(f"{filename}.html", "w", encoding="utf-8") as f:
+                    f.write(page_html)
+            if (
+                "Neste momento não temos imóveis com o perfil que está procurando"
+                in page_html
+            ):
+                _logger.info("No more pages to scrape!")
+                return None
             return page_html
 
     # TODO - Why scraped_at is not updated?
